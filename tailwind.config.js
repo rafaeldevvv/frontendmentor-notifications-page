095aa7ad25e1799d0db6490f79e12a459e490568
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2024-02-29 14:00:57 -0300

    chore: configure tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
   theme: {
+    colors: {
+      red: "hsl(1, 90%, 64%)",
+      blue: "hsl(219, 85%, 26%)",
+      white: "hsl(0, 0%, 100%)",
+      veryLightGrayishBlue: "hsl(210, 60%, 98%)",
+      lightGrayishBlue: {
+        "1": "hsl(211, 68%, 94%)",
+        "2": "hsl(205, 33%, 90%)"
+      },
+      grayishBlue: "hsl(219, 14%, 63%)",
+      darkGrayishBlue: "hsl(219, 12%, 42%)",
+      veryDarkBlue: "hsl(224, 21%, 14%)"
+    },
+    fontFamily: {
+      sans: ["var(--ff-sans)", ...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   plugins: [],
